f8c634b5618d34c8629067e1f5dddd6ae0046726
Hernan Pedrino <hernanpedrino@gmail.com>
2026-03-12 12:52:53 -0300

    Data Table

--- app/(pages)/insumos/page.tsx
+++ app/(pages)/insumos/page.tsx
@@ -1,11 +1,48 @@
-import { Table } from "@/components/table/Table";
+import { columns, Payment } from "./columns"
+import { DataTable } from "./data-table";
 
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 80,
+      status: "pending",
+      email: "rtrerp@example.com",
+    },
+    {
+      id: "754ed52f",
+      amount: 480,
+      status: "pending",
+      email: "nmba@example.com",
+    },
+    {
+      id: "792ed52f",
+      amount: 4500,
+      status: "pending",
+      email: "sghgg@example.com",
+    },
+    {
+      id: "359ed52f",
+      amount: 1500,
+      status: "pending",
+      email: "wevzxcm@example.com",
+    },
+    {
+      id: "299ed52f",
+      amount: 150,
+      status: "pending",
+      email: "serwsi@example.com",
+    },
+    // ...
+  ]
+}
+export default async function Insumos() {
+  const data = await getData()
 
-export default function Insumos() {
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="text-3xl m-2">Listado de insumos</h2>
-      <Table/>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
   )
 }
