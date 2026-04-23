03d5725f8b3b7c60ffd5de906ee9917d9290f8c0
Hernan Pedrino <hernanpedrino@gmail.com>
2026-04-23 08:45:55 -0300

    Arreglo del prisma client

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -2,12 +2,11 @@ import { prisma } from '@/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: Request) {
-  const inputs = await prisma.inputs.findMany()
+  const inputs = await prisma.inputs.findMany();
   return NextResponse.json(inputs)
 }
 export async function POST(request: Request) {
-
   const body = await request.json();
-  const newInput = await prisma.inputs.create({ data: body })
+  const newInput = await prisma.inputs.create({ data: body });
   return NextResponse.json(newInput);
 }
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,5 +5,10 @@ const adapter = new PrismaMariaDb({
   host: "localhost",
   port: 3306,
   connectionLimit: 5,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+
 });
 export const prisma = new PrismaClient({ adapter });
+
