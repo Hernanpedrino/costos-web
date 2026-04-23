36422ca4b83debd2ae150b9f0d240e503b6a00e9
Hernan Pedrino <hernanpedrino@gmail.com>
2026-04-23 11:30:36 -0300

    Arreglo de la fecha en la tabla insumos

--- app/(pages)/insumos/columns.tsx
+++ app/(pages)/insumos/columns.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef } from "@tanstack/react-table";
 export type Insumos = {
   name: string;
   id: string;
@@ -17,12 +17,33 @@ export const columns: ColumnDef<Insumos>[] = [
   },
   {
     accessorKey: "createdAt",
-    header: "Fecha de creacion",
+    header: "Fecha de carga",
+    cell: ({ row }) => {
+      const date: string = row.getValue("createdAt")
+      const dateObj = new Date(date);
+      const formateador = new Intl.DateTimeFormat('es-ES', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      });
+      const fechaFormateada = formateador.format(dateObj);
+      return fechaFormateada;
+    }
   },
   {
     accessorKey: "updatedAt",
-    header: "Fecha de ultima modificacion"
-    //TODO: Corregir el formato de la fecha
+    header: "Fecha de ultima modificacion",
+    cell: ({ row }) => {
+      const date: string = row.getValue("createdAt")
+      const dateObj = new Date(date);
+      const formateador = new Intl.DateTimeFormat('es-ES', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      });
+      const fechaFormateada = formateador.format(dateObj);
+      return fechaFormateada;
+    }
   },
   {
     accessorKey: "price",
